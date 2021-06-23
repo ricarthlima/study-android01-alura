545bbb7a1fb24a1d087d9640559cb4e7847273af
Ricarth Lima <rrsl@cin.ufpe.br>
2021-06-23 11:57:04 -0300

    listview

--- app/src/main/java/ricarth/alura/com/MainActivity.java
+++ app/src/main/java/ricarth/alura/com/MainActivity.java
@@ -2,15 +2,31 @@ package ricarth.alura.com;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
+        // Dados estáticos para exemplo
+        List<String> students = new ArrayList<String>(
+                Arrays.asList("Alex", "Fran", "Jose"));
+
+        ListView listViewStudents = findViewById(R.id.activity_main_list_students);
+
+        listViewStudents.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, students));
+
     }
 }
